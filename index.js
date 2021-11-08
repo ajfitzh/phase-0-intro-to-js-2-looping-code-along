c86a175ef318434650d0a2fdbe928e2aaa6cf30a
Austin <austin.j.fitzhugh@gmail.com>
2021-11-08 14:14:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names,eventName){
+    for(let i=0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+ return messages;
+}
+
+function countDown(int){
+    let i=10;
+    while(i>=0) {
+        console.log(i);
+        i--;
+    }
+    return int;
+}
+
+
